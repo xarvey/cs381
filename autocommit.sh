3bbd86a29491571fdab839b0048924070649eb4f
Yao Xiao <xiao67@purdue.edu>
2014-12-11 20:54:37 -0500

    Code was clean until manager requested to fuck it up

---
+++ autocommit.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+# git autocommit script.
+# Checks to see if there has been a commit in the last preset timeframe.
+# If there hasn't been, it automatically commits changes to source control.
+
+# Usage: autocommit.sh <timeframe>
+#
+# <timeframe> is any time or date token accepted by the git log --since option.
+
+git pull -q
+git add --all 
+git commit -q -a -m"`curl -s http://whatthecommit.com/index.txt`"
+git push -q
